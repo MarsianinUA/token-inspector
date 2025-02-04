e3a9e2e402a6129eb589c79e11fb95b63690004b
Denis <62204087+MarsianinUA@users.noreply.github.com>
2025-02-04 19:53:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const ethers = require('ethers');
-const ABI = require('./abis/ERC20.abi.json');
+const ABI = require('./abis/erc-20.abi.json');
 
 const TOKEN_ADDRESS = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
 const RPC_URL = 'https://eth.llamarpc.com';
